1c336f7fe80b53dfd2ad88b32ee5fa278ce01f1a
papi0109 <nikaidone@gmail.com>
2025-08-30 18:05:15 +0900

    API_URL修正

--- assets/config.js
+++ assets/config.js
@@ -3,5 +3,5 @@
 window.__APP_CONFIG__ = {
   // Example (GAS Web App): "https://script.google.com/macros/s/XXXX/exec"
   // This should be the full URL that returns the portfolio JSON.
-  API_URL: ""
+  API_URL: "https://script.google.com/macros/s/AKfycby1bahiz5z1CBsOeBIHUHO-dFoKUpgi6GAcySe1kbSLXwQtgilk14bwck3mRDS8kTR-ig/exec?admin=1"
 };
